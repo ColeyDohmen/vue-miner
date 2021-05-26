ec634a54d9ff1b938da93cb904ccfb4b2000d7ab
ColeyDohmen <coley.dawn12@gmail.com>
2021-05-26 13:19:33 -0600

    background gif

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,10 +1,15 @@
 <template>
   <div class="home flex-grow-1 d-flex flex-column align-items-center justify-content-center">
-    <!-- <img src="https://bcw.blob.core.windows.net/public/img/8600856373152463" alt="CodeWorks Logo"> -->
-    <!-- <h1 class="my-5 bg-dark text-light p-3 rounded d-flex align-items-center">
+    <div id="back" class="back">
+      <!-- <img src="https://bcw.blob.core.windows.net/public/img/8600856373152463" alt="CodeWorks Logo"> -->
+      <!-- <h1 class="my-5 bg-dark text-light p-3 rounded d-flex align-items-center">
       <span class="mx-2 text-white">Vue Miner</span>
     </h1> -->
-    <Moon />
+      <h1 class="text-light">
+        Moon Miner
+      </h1>
+      <Moon />
+    </div>
   </div>
 </template>
 
@@ -22,5 +27,13 @@ export default {
     height: 200px;
     width: 200px;
   }
+
 }
+ .back {
+    background-image: url('https://d.wattpad.com/story_parts/676761663/images/15856e540cd2e8bd234625630146.gif');
+    background-size: cover;
+    }
+  // .back {
+  //   background-image: url('https://image.freepik.com/free-vector/background-circuit-board-neon_23-2148351172.jpg');
+  //   }
 </style>
