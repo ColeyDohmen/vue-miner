6c76152c70e84c504e5e23e9d2c2aac5167a2588
ColeyDohmen <coley.dawn12@gmail.com>
2021-05-27 16:12:08 -0600

    fixed background

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="home flex-grow-1 d-flex flex-column align-items-center justify-content-center">
-    <div id="back" class="back">
+    <div id="back" class="back container-fluid">
       <!-- <img src="https://bcw.blob.core.windows.net/public/img/8600856373152463" alt="CodeWorks Logo"> -->
       <!-- <h1 class="my-5 bg-dark text-light p-3 rounded d-flex align-items-center">
       <span class="mx-2 text-white">Vue Miner</span>
@@ -8,12 +8,12 @@
       <h1 class="text-light">
         Moon Miner
       </h1>
+      <h4 class=" align-items-center justify-content-center text-light float-left">
+        Moon Dust:<div id="moonDust"></div>
+      </h4>
       <Moon />
     </div>
   </div>
-  <h4>
-    Moon Dust:<div id="moonDust"></div>
-  </h4>
 </template>
 
 <script>
