5d1f24dd2b5f004f3a6fdd91e6960c710fa6444b
ColeyDohmen <coley.dawn12@gmail.com>
2021-05-28 12:50:23 -0600

    update

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -4,13 +4,20 @@
       <!-- <img src="https://bcw.blob.core.windows.net/public/img/8600856373152463" alt="CodeWorks Logo"> -->
       <!-- <h1 class="my-5 bg-dark text-light p-3 rounded d-flex align-items-center">
       <span class="mx-2 text-white">Vue Miner</span>
-    </h1> -->
+    </h1> -->.
       <h1 class="text-light">
         Moon Miner
       </h1>
-      <h4 class=" align-items-center justify-content-center text-light float-left">
-        Moon Dust:<div id="moonDust"></div>
-      </h4>
+      <div class="row">
+        <h4 class=" align-items-center justify-content-center text-light float-left">
+          Moon Dust:<div id="moonDust"></div>
+        </h4>
+        <div class="col-3">
+          <h4 class=" align-items-center justify-content-center text-light float-left">
+            Upgrades:
+          </h4>
+        </div>
+      </div>
       <Moon />
     </div>
   </div>
